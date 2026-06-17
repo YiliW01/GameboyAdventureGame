285d53fbd48aaec8c1a8cd94ebe0f5270f79401b
Yi Wang <s2533013@kpuenta.ca>
2026-06-16 23:13:58 -0700

    audio setup done

---
+++ gameboyAdventure/Assets/Scripts/PlayMusicOnStart.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class PlayMusicOnStart : MonoBehaviour
+{
+    private void Start()
+    {
+        AudioMgr.Instance.PlayMusic(AudioMgr.MusicType.Gameplay, 1f);
+    }
+}
